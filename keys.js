aeb78d80cdae60a70990da63d6623d5953e7d9d9
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2017-05-21 12:37:15 -0700

    initial asset commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'MOnO7pbs2fCi937BV8lQQjdtG',
+  consumer_secret: 'fxn6lu6pf4HrdpAT59td9VMfK6UIkfcGCsotHFvvRUgSLfVfDs',
+  access_token_key: '	866367295685246976-PUK1Lp5zqGggykZcknMT0AeOivZzWvp',
+  access_token_secret: '4ZBRDx0HRfrdtOL5QbzAWX9wcasq2lfE71KjJGMwVgsOq',
+}
---
+++ liri.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var twitter = require('twitter');
+var spotify = require('spotify');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var twitterkeys = require('./keys.js');
+
+
+
+// Command: node liri.js my-tweets
+    // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+    
+
+// Command:  node liri.js spotify-this-song '<song name here>'
+    // This will show the following information about the song in your terminal/bash window
+    // Artist(s)
+    // The song's name
+    // A preview link of the song from Spotify
+    // The album that the song is from
+    // if no song is provided then your program will default to
+    // "The Sign" by Ace of Base
+
+
+// Command:  node liri.js movie-this '<movie name here>'
+    // This will output the following information to your terminal/bash window:
+    //    * Title of the movie.
+    //    * Year the movie came out.
+    //    * IMDB Rating of the movie.
+    //    * Country where the movie was produced.
+    //    * Language of the movie.
+    //    * Plot of the movie.
+    //    * Actors in the movie.
+    //    * Rotten Tomatoes URL.
+    // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+
+// Command:  node liri.js do-what-it-says
+    // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+    // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+    // Feel free to change the text in that document to test out the feature for other commands.
+
+
+// In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
+    // Make sure you append each command you run to the log.txt file.
+    // Do not overwrite your file each time you run a command.
